3a0ba90661926ddbfbe4859baf17d668c52dd0b3
Pedro Castaneda <petecast76@gmail.com>
2024-01-16 00:11:32 -0800

    added logic for player socres

--- app.js
+++ app.js
@@ -2,10 +2,40 @@ const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const p1Score = document.querySelector('#p1Score');
 const p2Score = document.querySelector('#p2Score');
+const reset = document.querySelector('#reset');
 
 let playerOneScore = 0;
 let playerTwoScore = 0;
 
+let winningScore = 5;
+let isGameOver = false;
+
 p1Button.addEventListener('click', function () {
-    
+    if (!isGameOver) {
+        playerOneScore++;
+        if (playerOneScore === winningScore) {
+            isGameOver = true;
+        }
+        p1Score.textContent = playerOneScore;
+    }
 });
+
+p2Button.addEventListener('click', function () {
+    if (!isGameOver) {
+        playerTwoScore++;
+        if (playerTwoScore === winningScore) {
+            isGameOver = true;
+        }
+        p2Score.textContent = playerTwoScore;
+    }
+});
+
+reset.addEventListener('click', resetGame);
+
+function resetGame() {
+    isGameOver = false;
+    playerOneScore = 0;
+    playerTwoScore = 0;
+    p1Score.textContent = 0;
+    p2Score.textContent = 0;
+}
